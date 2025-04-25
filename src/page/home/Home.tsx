b954e9ffc25483970508c79de2ba205cb2f9817d
kitik <svy1dan@gmail.com>
2025-04-25 20:03:59 +0300

    page directory

---
+++ src/page/home/Home.tsx
@@ -0,0 +1,13 @@
+import Header from '../../UI compnent/Header/Header.tsx'
+import Title from '../../UI compnent/name and profession title/Title.tsx'
+import Logo from '../../UI compnent/title logo/Logo.tsx'
+
+export default function Home() {
+    return (
+        <>
+            <Header />
+            <Title />
+            <Logo/>
+        </>
+    )
+}
---
+++ src/page/technologies page/Technologies.tsx
@@ -0,0 +1,13 @@
+import './technologies.css';
+
+import Header from '../../UI compnent/Header/Header.tsx'
+
+export default function Technologies () {
+    return (
+        <>
+            <div>
+                <Header/>
+            </div>
+        </>
+    )
+}
