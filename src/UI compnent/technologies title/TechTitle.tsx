ba286baf00917860c73650bb7ce7718c819a073d
kitik <svy1dan@gmail.com>
2025-04-27 21:56:10 +0300

    technologies title

---
+++ src/UI compnent/technologies title/TechTitle.tsx
@@ -0,0 +1,12 @@
+import './techTitle.css';
+
+export default function TechTitle() {
+    return (
+        <>
+            <div className="techTitle">
+                <h1 className="text-gradient text-[40px] font-bold capitalize">My skills</h1>
+                <h2 className="text-gradient text-[40px] font-bold capitalize">(languages, libraries, frameworks and more)</h2>
+            </div>
+        </>
+    )
+}
