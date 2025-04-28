8295774bae79bfc524f1ed9ab1e0c7e098ed20c4
kitik <svy1dan@gmail.com>
2025-04-28 21:30:54 +0300

    main file on project

--- src/main.tsx
+++ src/main.tsx
@@ -8,9 +8,9 @@ import {MusicProvider} from "./contexts/music context/MusicContext.tsx";
 import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-      <MusicProvider>
-          <App />
-      </MusicProvider>
-  </StrictMode>,
+    <StrictMode>
+        <MusicProvider>
+                <App />
+        </MusicProvider>
+    </StrictMode>,
 )
