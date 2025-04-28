3ecc8c63e8f2ab7bfac24acc7b167e14d19705a6
kitik <svy1dan@gmail.com>
2025-04-28 21:29:02 +0300

    UI components for contact page

---
+++ src/UI compnent/componets for contact/contact title/ConTitle.tsx
@@ -0,0 +1,11 @@
+import './conTitle.css';
+
+export default function ConTitle() {
+    return (
+        <>
+            <div className="contactTitleMain">
+                 <h1 className="text-gradient text-[40px] font-bold capitalize">My contacts</h1>
+            </div>
+        </>
+    )
+}
