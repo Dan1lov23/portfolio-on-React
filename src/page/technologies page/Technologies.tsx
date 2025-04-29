d7f8240338113f56ce91bfee639a84c3ca721cab
kitik <svy1dan@gmail.com>
2025-04-29 22:44:08 +0300

    technologies component

--- src/page/technologies page/Technologies.tsx
+++ src/page/technologies page/Technologies.tsx
@@ -1,8 +1,8 @@
 import './technologies.css';
 
-import Header from '../../UI compnent/Header/Header.tsx';
-import TechTitle from '../../UI compnent/technologies title/TechTitle.tsx'
-import MainStack from '../../UI compnent/technologies main stack/MainStack.tsx';
+import Header from '../../UI compnent/components for all/Header/Header.tsx';
+import TechTitle from '../../UI compnent/components for skills/technologies title/TechTitle.tsx'
+import MainStack from '../../UI compnent/components for skills/technologies main stack/MainStack.tsx';
 
 export default function Technologies() {
     return (
