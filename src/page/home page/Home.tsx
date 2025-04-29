2a282e967dda27681534880130450a07ab95bfa7
kitik <svy1dan@gmail.com>
2025-04-29 22:43:23 +0300

    main file on project

--- src/page/home page/Home.tsx
+++ src/page/home page/Home.tsx
@@ -1,6 +1,6 @@
-import Header from '../../UI compnent/Header/Header.tsx'
-import Title from '../../UI compnent/name and profession title/Title.tsx'
-import Logo from '../../UI compnent/title logo/Logo.tsx'
+import Header from '../../UI compnent/components for all/Header/Header.tsx'
+import Title from '../../UI compnent/compoents for home/name and profession title/Title.tsx'
+import Logo from '../../UI compnent/compoents for home/title logo/Logo.tsx'
 
 export default function Home() {
     return (
