27e4f9fcc85f10c05118b277584375ccd6ee8fb3
kitik <svy1dan@gmail.com>
2025-04-29 22:42:59 +0300

    contact page

--- src/page/contact page/Contact.tsx
+++ src/page/contact page/Contact.tsx
@@ -1,8 +1,8 @@
 import './contact.css';
 
-import Header from '../../UI compnent/Header/Header.tsx';
-import ConTitle from '../../UI compnent/contact title/ConTitle.tsx';
-import ContactsIcon from '../../UI compnent/contacts icons/ContactsIcons.tsx';
+import Header from '../../UI compnent/components for all/Header/Header.tsx';
+import ConTitle from '../../UI compnent/componets for contact/contact title/ConTitle.tsx';
+import ContactsIcon from '../../UI compnent/componets for contact/contacts icons/ContactsIcons.tsx';
 
 export default function Contact() {
     return (
