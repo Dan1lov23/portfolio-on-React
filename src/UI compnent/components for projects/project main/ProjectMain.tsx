5390dcc7014201ba339b5de54e3e9801b1cdd5ca
kitik <svy1dan@gmail.com>
2025-05-02 21:40:39 +0300

    project main

--- src/UI compnent/components for projects/project main/ProjectMain.tsx
+++ src/UI compnent/components for projects/project main/ProjectMain.tsx
@@ -3,7 +3,7 @@ import './projectMain.css';
 import ShowAppButton from "../show code button/ShowAppButton.tsx";
 import ShowCodeButton from "../show app button/ShowCodeButton.tsx";
 
-export default function ProjectMain({img, title, appSrc, codeSrc}) {
+export default function ProjectMain({img, title, appSrc, codeSrc}:{img:string, title:string, appSrc:string, codeSrc:string}) {
     return (
         <>
             <div className="projectMain">
