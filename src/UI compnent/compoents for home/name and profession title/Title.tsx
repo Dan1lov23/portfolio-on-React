8ed44cd7714c9d8caaf18a70e9f0179ecfe2e3dd
kitik <svy1dan@gmail.com>
2025-05-02 21:39:48 +0300

    name and profession title

---
+++ src/UI compnent/compoents for home/name and profession title/Title.tsx
@@ -0,0 +1,16 @@
+import './title.css';
+
+export default function Title() {
+    return (
+        <>
+            <div className="titleMain">
+                <h1 className="text-gradient text-[40px] font-bold capitalize">
+                    Svatoslav Danilov
+                </h1>
+                <h2 className="text-gradient text-[40px] font-bold capitalize">
+                    frontend web-developer
+                </h2>
+            </div>
+        </>
+    )
+}
